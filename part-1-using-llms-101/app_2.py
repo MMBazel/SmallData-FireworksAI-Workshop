b7038187f01affe084cf0130af26307ee28e6212
Mikiko Bazeley <mikiko_fw@Mikikos-MacBook-Pro.local>
2024-09-30 11:06:54 -0700

    Creating new structure for multipage app

---
+++ part-1-using-llms-101/app_2.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(page_title="Fireworks LLM Playground", page_icon="🎇")
+
+st.title("Welcome to Fireworks AI Playground")
+st.write("Navigate between pages to explore different features.")
